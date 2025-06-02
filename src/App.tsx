6ed0576778317ae3a50cad06f806ebace665f6fe
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2025-06-02 18:42:41 +0330

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PageNotFound from "./pages/PageNotFound";
+import User from "./pages/User";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 function App() {
-  return <h1>Axon project</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="users" />} />
+          <Route path="users" element={<Users />} />
+          <Route path="users/:userId" element={<User />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
