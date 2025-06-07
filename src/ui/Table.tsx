90fc0e4f58709588e2f6f61c6591dd49ed7f79e8
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2025-06-07 14:02:29 +0330

    Add users table to TableRow component

--- src/ui/Table.tsx
+++ src/ui/Table.tsx
@@ -8,9 +8,28 @@ function Table() {
   if (error) return <Error>There was an error while fetching data</Error>;
   if (isPending) return <Spinner />;
   return (
-    <div>
-      <h1>Users data table</h1>
-      {Users?.map((user) => <TableRow user={user} />)}
+    <div className="overflow-x-auto rounded-lg font-nunito">
+      <table className="text-left">
+        <thead className="bg-violet-100">
+          <tr>
+            <th className="px-4 py-2">ID</th>
+            <th className="px-4 py-2">Name</th>
+            <th className="px-4 py-2">Age</th>
+            <th className="px-4 py-2">Job</th>
+            <th className="px-4 py-2">Income</th>
+            <th className="px-4 py-2">Country</th>
+            <th className="px-4 py-2">Gender</th>
+            <th className="px-4 py-2">National ID</th>
+            <th className="px-4 py-2">Married</th>
+            <th className="px-4 py-2">Child</th>
+            <th className="px-4 py-2">Image</th>
+            <th className="px-4 py-2">Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Users?.map((user) => <TableRow key={user.id} user={user} />)}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/ui/TableRow.tsx
+++ src/ui/TableRow.tsx
@@ -19,6 +19,27 @@ type TableRowProps = {
 };
 
 function TableRow({ user }: TableRowProps) {
-  return <h1>{user.name}</h1>;
+  return (
+    <tr className="border-b *:px-3 *:py-0.5 hover:bg-violet-50/85">
+      <td>{user.id < 10 ? `0${user.id}` : user.id}</td>
+      <td>{user.name}</td>
+      <td>{user.age ?? "-"}</td>
+      <td>{user.job ?? "-"}</td>
+      <td>{user.income ?? "-"}</td>
+      <td>{user.country ?? "-"}</td>
+      <td>{user.gender ?? "-"}</td>
+      <td>{user.national_id}</td>
+      <td>{String(user.isMarried ?? "-")}</td>
+      <td>{user.child ?? "-"}</td>
+      <td>
+        <img
+          src={user.image ?? "/User.png"}
+          alt={`${user.name} picture`}
+          className="w-14 rounded-lg"
+        />
+      </td>
+      <td>{user.role}</td>
+    </tr>
+  );
 }
 export default TableRow;
