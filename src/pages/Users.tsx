c1081510e726c2d05ca395a0f25f1ef334ff614c
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2025-06-09 16:30:28 +0330

    Add FilterSection component

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,4 +1,5 @@
 import { useUsers } from "@/hooks/useUsers";
+import FilterSection from "@/ui/Filters";
 import SearchInput from "@/ui/SearchInput";
 import Spinner from "@/ui/Spinner";
 import Table from "@/ui/Table";
@@ -6,14 +7,41 @@ import { useState } from "react";
 
 function Users() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [filters, setFilters] = useState({
+    country: "",
+    gender: "",
+    isMarried: "",
+    role: "",
+  });
   const { Users, isPending, error } = useUsers();
-  const filteredUsers = Users?.filter((user) =>
-    user.name.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
+  const filteredUsers = Users?.filter((user) => {
+    const matchesSearch = user.name
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+
+    const matchesCountry = filters.country
+      ? user.country === filters.country
+      : true;
+    const matchesGender = filters.gender
+      ? user.gender === filters.gender
+      : true;
+    const matchesMarried = filters.isMarried
+      ? String(user.isMarried) === filters.isMarried
+      : true;
+    const matchesRole = filters.role ? user.role === filters.role : true;
+    return (
+      matchesSearch &&
+      matchesCountry &&
+      matchesGender &&
+      matchesMarried &&
+      matchesRole
+    );
+  });
   if (isPending) return <Spinner />;
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <FilterSection filters={filters} setFilters={setFilters} />
       <Table Users={filteredUsers} error={error} />
     </div>
   );
